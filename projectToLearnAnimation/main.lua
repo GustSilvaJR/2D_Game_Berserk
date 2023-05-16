239b1fe0cf51db0890353c90d0a848090e0b58c1
GustSilvaJR <gust.offic@gmail.com>
2023-05-16 17:05:39 -0300

    Iniciando projeto jack walk, criando propriedades, criando sprite, e definindo altura e largura da imagem

---
+++ projectToLearnAnimation/main.lua
@@ -0,0 +1,24 @@
+_G.love = require("love")
+
+function love.load()
+    jack = {
+        x = 0,
+        y = 0,
+        sprite = {
+            image = love.graphics.newImage('sprites/jackWalk.png'),
+            width = 5352,
+            height = 569
+        }
+    }
+
+    
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
